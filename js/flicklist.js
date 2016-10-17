0856fcb6f7a031b24b8cff7b0a20a5c7a1b59e02
robpaulnett <paulrobnett@hotmail.com>
2016-10-17 17:49:19 -0500

    studio 0 upload

--- js/flicklist.js
+++ js/flicklist.js
@@ -2,7 +2,7 @@
 
 var api = {
 	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
+	token: "../../../../../../api_keys/themoviedb.txt" // TODO put your api key here
 }
 
 /**
